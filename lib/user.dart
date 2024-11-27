8c96238721d217b078ff865112e62e5dc8aa37a5
Rawan Sayed <rawansayed262@gmail.com>
2024-11-27 14:37:13 +0200

    Update user.dart

--- lib/user.dart
+++ lib/user.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:glucoograph/components/bar_chart/chart_graph.dart';
 import 'package:glucoograph/connection/connection.dart';
 import 'package:glucoograph/constants/constants.dart';
-import 'package:glucoograph/components/bar_chart/GraphAllIndicator.dart';
 import 'package:glucoograph/graphs_type.dart';
 
 class User extends StatefulWidget {
@@ -26,7 +24,7 @@ class _UserState extends State<User> {
   Widget build(BuildContext context) {
     return FutureBuilder<List<Map<String, dynamic>>>(
       future: DatabaseHelper.fetchPatientNamesAndGender(
-          filterAbove80: _filterAbove80), // تمرير حالة الفلتر
+           filterAbove80: _filterAbove80), // تمرير حالة الفلتر
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return Center(
@@ -53,22 +51,25 @@ class _UserState extends State<User> {
               ),
               actions: [
                 IconButton(
-                  icon: Icon(
-                    _filterAbove80 ? Icons.filter_list_off : Icons.filter_list,
-                  ),
-                  color: Colors.white,
-                  onPressed: _toggleFilter, // عند الضغط نقوم بتغيير الفلتر
-                ),
+                    icon: Icon(
+                      _filterAbove80
+                          ? Icons.filter_list_off
+                          : Icons.filter_list,
+                    ),
+                    color: Colors.white,
+                    onPressed:
+                        _toggleFilter, // عند الضغط نقوم بتغيير الفلتر
+                    ),
               ],
             ),
             body: Padding(
               padding: const EdgeInsets.all(10),
               child: GridView.builder(
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3, // عدد الأعمدة
+                  crossAxisCount: 1, // عدد الأعمدة
                   mainAxisSpacing: 10, // المسافة بين العناصر في الاتجاه الرأسي
                   crossAxisSpacing: 10, // المسافة بين العناصر في الاتجاه الأفقي
-                  childAspectRatio: 0.7, // تحديد النسبة بين العرض والارتفاع
+                  childAspectRatio: 3, // تحديد النسبة بين العرض والارتفاع
                 ),
                 itemCount: users.length, // استخدم المستخدمين المفلترين
                 itemBuilder: (context, index) {
@@ -76,7 +77,11 @@ class _UserState extends State<User> {
                   final imagePath = user['gender'] == 'Male'
                       ? 'assets/male.jpg'
                       : 'assets/female.jpg';
-
+                  double healthStatus =
+                      double.tryParse(user['health_status'] ?? '0') ??
+                          0; // تحويل حالة الصحة
+                  bool showAdditionalContainer =
+                      healthStatus > 90; // التحقق إذا كانت أكبر من 90
                   return InkWell(
                     onTap: () {
                       Navigator.push(
@@ -95,28 +100,65 @@ class _UserState extends State<User> {
                               BorderRadius.circular(15), // حواف دائرية
                         ),
                         elevation: 3, // إضافة الظل
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            CircleAvatar(
-                              radius: 40, // حجم الصورة
-                              backgroundImage: AssetImage(imagePath),
-                            ),
-                            SizedBox(height: 10),
-                            Container(
-                              height: 50,
-                              child: Text(
-                                user['name'],
-                                style: TextStyle(
-                                  fontFamily: kPrimaryFont,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                                textAlign: TextAlign.center,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              CircleAvatar(
+                                radius: 40, // حجم الصورة
+                                backgroundImage: AssetImage(imagePath),
                               ),
-                            ),
-                          ],
+                              Container(
+                                height: 60,
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      user['name'],
+                                      style: TextStyle(
+                                        fontFamily: kPrimaryFont,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                    SizedBox(
+                                      height: 8,
+                                    ),
+                                    Text(
+                                      'Age: ${user["age"].toString()}',
+                                      style: TextStyle(
+                                        fontFamily: kPrimaryFont,
+                                        fontSize: 15,
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              if (showAdditionalContainer)
+                                Container(
+                                  width: 75,
+                                  height: 30,
+                                  decoration: BoxDecoration(
+                                    color:
+                                        const Color.fromARGB(255, 165, 27, 18),
+                                    borderRadius: BorderRadius.circular(25),
+                                  ),
+                                  child: Center(
+                                      child: Text('Critical',
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                          ))),
+                                )
+                              else
+                                Container(
+                                  width: 75,
+                                  height: 30,
+                                )
+                            ],
+                          ),
                         ),
                       ),
                     ),
